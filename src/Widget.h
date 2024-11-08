86000767fb5d2ec1a4254f13282c529ad06a8ea5
Erkan Çolak <erkanc@gmx.de>
2024-11-08 21:59:54 +0100

    Removed convertCharToCP437! Since we will not implement umlaut support for now.

--- src/Widget.h
+++ src/Widget.h
@@ -1,5 +1,9 @@
 #pragma once
 
+// WIDGETS
+#define QRCODE_WIDGET      // Enable the QR code widget
+#define MATRIX_SCREENSAVER // Enable the matrix screensaver
+
 #include "i2c-Display.h"  // Include 1st
 #include "DisplayIcons.h" // Include 2nd
 #ifdef QRCODE_WIDGET
@@ -16,10 +20,6 @@
 
 // Default settings for the display
 #define MAX_TEXT_LINES 8 // Maximum number of text lines on a 128x64 display with default font
-
-// WIDGETS
-//#define QRCODE_WIDGET      // Enable the QR code widget
-//#define MATRIX_SCREENSAVER // Enable the matrix screensaver
 
 // Matrix screensaver settings
 #ifdef MATRIX_SCREENSAVER
@@ -96,7 +96,6 @@ class Widget
         SCREEN_SAVER, // Matrix screensaver
 #endif
         BOOT_LOGO // Boot logo
-
     };
 
   private:
@@ -106,55 +105,39 @@ class Widget
     // Text lines for dynamic text mode
     bool _AllowEmtyTextLines = false; // Flag to enable initial start with empty lines. Default is false. I.e. to fill lines later
 
-    uint16_t getTextWidth(i2cDisplay *display, const char *text, uint8_t textSize);             // Get the width of the text in pixels
-    uint16_t getTextHeight(i2cDisplay *display, const char *text, uint8_t textSize);            // Get the height of the text in pixels
-    uint16_t calculateMaxTextLines(i2cDisplay *display, const GFXfont *font = nullptr);         // Calculate the maximum number of text lines
-    void writeScrolledText(i2cDisplay *display, const char *text, int scrollPos, int maxChars); // Write the scrolled text to the display
-    bool checkAndUpdateLcdText(lcdText *sText);                                                 // Check and update the text on the display
-    void displayDynamicText(i2cDisplay *display, const std::vector<lcdText *> &lines);          // Display the dynamic text on the display
-    void InitDynamicTextLines();                                                                // Initialize the dynamic text lines with default settings
-    void UpdateDynamicTextLines(i2cDisplay *display);                                           // Update the dynamic text lines on the display
+    uint16_t getTextWidth(i2cDisplay * display, const char *text, uint8_t textSize);             // Get the width of the text in pixels
+    uint16_t getTextHeight(i2cDisplay * display, const char *text, uint8_t textSize);            // Get the height of the text in pixels
+    uint16_t calculateMaxTextLines(i2cDisplay * display, const GFXfont *font = nullptr);         // Calculate the maximum number of text lines
+    void writeScrolledText(i2cDisplay * display, const char *text, int scrollPos, int maxChars); // Write the scrolled text to the display
+    bool checkAndUpdateLcdText(lcdText * sText);                                                 // Check and update the text on the display
+    void displayDynamicText(i2cDisplay * display, const std::vector<lcdText *> &lines);          // Display the dynamic text on the display
+    void InitDynamicTextLines();                                                                 // Initialize the dynamic text lines with default settings
+    void UpdateDynamicTextLines(i2cDisplay * display);                                           // Update the dynamic text lines on the display
 
     // Helper functions for displayDynamicText
-    void calculateTextHeights(i2cDisplay *display, const std::vector<lcdText *> &textLines, uint16_t &totalHeightTop, uint16_t &totalHeightBottom, uint16_t &totalMiddleHeight, uint16_t &middleLineCount);             // Calculate the heights of the text sections
-    void drawTextLines(i2cDisplay *display, const std::vector<lcdText *> &textLines, uint16_t totalHeightTop, uint16_t totalHeightBottom, uint16_t middleStartY, uint16_t availableMiddleHeight, uint32_t currentTime); // Draw each line of text
-    void handleScrolling(i2cDisplay *display, lcdText *line, uint32_t currentTime);                                                                                                                                     // Handle the scrolling of a text line
-    uint16_t calculateCursorX(i2cDisplay *display, const lcdText *line);                                                                                                                                                // Calculate the X position of the cursor for a text line
-    uint16_t calculateCursorY(i2cDisplay *display, const lcdText *line, uint16_t &totalHeightTop, uint16_t &totalHeightBottom, uint16_t &middleStartY, uint16_t availableMiddleHeight);                                 // Calculate the Y position of the cursor for a text line
+    void calculateTextHeights(i2cDisplay * display, const std::vector<lcdText *> &textLines, uint16_t &totalHeightTop, uint16_t &totalHeightBottom, uint16_t &totalMiddleHeight, uint16_t &middleLineCount);             // Calculate the heights of the text sections
+    void drawTextLines(i2cDisplay * display, const std::vector<lcdText *> &textLines, uint16_t totalHeightTop, uint16_t totalHeightBottom, uint16_t middleStartY, uint16_t availableMiddleHeight, uint32_t currentTime); // Draw each line of text
+    void handleScrolling(i2cDisplay * display, lcdText * line, uint32_t currentTime);                                                                                                                                    // Handle the scrolling of a text line
+    uint16_t calculateCursorX(i2cDisplay * display, const lcdText *line);                                                                                                                                                // Calculate the X position of the cursor for a text line
+    uint16_t calculateCursorY(i2cDisplay * display, const lcdText *line, uint16_t &totalHeightTop, uint16_t &totalHeightBottom, uint16_t &middleStartY, uint16_t availableMiddleHeight);                                 // Calculate the Y position of the cursor for a text line
 
     // Boot logo and OpenKNX logo
-    void OpenKNXLogo(i2cDisplay *display);  // Show the OpenKNX logo on the display
-    void ShowBootLogo(i2cDisplay *display); // Show the boot logo on the display
+    void OpenKNXLogo(i2cDisplay * display);  // Show the OpenKNX logo on the display
+    void ShowBootLogo(i2cDisplay * display); // Show the boot logo on the display
 
     // Programming mode
-    ulong _showProgrammingMode_last_Blink = 0;     // Last time the blink state was updated
-    bool _showProgrammingMode_showProgMode = true; // Toggle between showing/hiding "Prog Mode active"
-    void showProgrammingMode(i2cDisplay *display); // Show the programming mode on the display
+    ulong _showProgrammingMode_last_Blink = 0;      // Last time the blink state was updated
+    bool _showProgrammingMode_showProgMode = true;  // Toggle between showing/hiding "Prog Mode active"
+    void showProgrammingMode(i2cDisplay * display); // Show the programming mode on the display
 
 #ifdef MATRIX_SCREENSAVER
     // Matrix screensaver
-    ulong _lastUpdateScreenSaver = 0;                // Last time the screensaver was updated
-    void showMatrixScreensaver(i2cDisplay *display); // Show the matrix screensaver on the display
+    ulong _lastUpdateScreenSaver = 0;                 // Last time the screensaver was updated
+    void showMatrixScreensaver(i2cDisplay * display); // Show the matrix screensaver on the display
 #endif
 #ifdef QRCODE_WIDGET
-    void showQRCode(i2cDisplay *display);
+    void showQRCode(i2cDisplay * display); // Show the QR code on the display
 #endif // QRCODE_WIDGET
-
-    // Convert special characters to CP437. Supports only with another font!
-    // inline uint8_t convertCharToCP437(uint8_t ch) {
-    //    switch (ch) {
-    //        case 0xC4: return 0x8E; // Ä
-    //        case 0xD6: return 0x99; // Ö
-    //        case 0xDC: return 0x9A; // Ü
-    //        case 0xE4: return 0x84; // ä
-    //        case 0xF6: return 0x94; // ö
-    //        case 0xFC: return 0x81; // ü
-    //        case 0xDF: return 0xE1; // ß
-    //        case 0xC7: return 0x80; // Ç
-    //        case 0xE7: return 0x87; // ç
-    //        default: return ch;     // All other characters are returned as is
-    //    }
-    //}
 
   public:
     inline void setAllowEmptyTextLines(bool empty) { _AllowEmtyTextLines = empty; } // Set the initial empty text lines flag
